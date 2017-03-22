18ecf96aa2e98f50023dca44dbedbe454e31841b
Jerry Udensi <Jerry.un@icloud.com>
2017-03-23 03:19:13 +0800

    Initial Commit

---
+++ JPS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  JPS
+//
+//  Created by Jerry U. on 3/23/17.
+//  Copyright © 2017 Lyshnia Limited. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
